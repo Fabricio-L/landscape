a6b636e382ccd23c461df96d518783abf1349b45
Fabricio Lukestik <fabri.lkt@gmail.com>
2023-03-28 19:43:44 -0300

    feat(pokedex): Add new page and styles

---
+++ src/pages/pokedex.js
@@ -0,0 +1,9 @@
+import styles from '@/styles/Pokedex.module.css'
+
+export default function pokedex(){
+  return (
+    <main className={styles.main}>
+        <h1>Next App</h1>
+      </main>
+  )
+}
