1e8c6fe7a74e462022ff8b7f6f6d81554bd3cdd8
Fabricio Lukestik <fabri.lkt@gmail.com>
2023-03-31 18:14:11 -0300

    feat(topbar): Add styles and dinamic links

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,50 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import styles from './Topbar.module.css'
+
+export default function Topbar() {
+  const router = useRouter()
+  const path = router.asPath
+
+  console.log(path)
+
+  const links = {
+    home: '/',
+    pokedex: '/pokedex',
+  }
+
+  console.log(path.split('/')[2])
+
+  return (
+    <section className={styles.section}>
+      <ul className={styles.links}>
+        <li>
+          <Link
+            href={links['home']}
+            className={links['home'] === path ? styles.active : styles.default}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={links['pokedex']}
+            className={
+              links['pokedex'] === path ? styles.active : styles.default
+            }
+          >
+            Pokedex
+          </Link>
+        </li>
+        <li className={path.split('/')[2] ? styles.active : styles.disable}>
+          <Link
+            href={path}
+            className={path === path ? styles.active : styles.default}
+          >
+            {path.split('/')[2]}
+          </Link>
+        </li>
+      </ul>
+    </section>
+  )
+}
